65f81f4a018862ee7a3a9014a044d01a59b5636b
Peter C. Norton <pcn@librato.com>
2016-09-18 01:01:51 -0400

    Turned most of this into functions.

--- ntpq.py
+++ ntpq.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-
+# pragma pylint: disable=bad-whitespace
 ###############################################################################
 # ntplib - Python NTP library.
 # Copyright (C) 2009 Charles-Francois Natali <neologix@free.fr>
@@ -33,263 +33,39 @@ import time
 
 import ntplib
 
+NTP_CONTROL_PACKET_FORMAT = "!B B H H H H H" #Maybe, maybe not
+"""packet format to pack/unpack"""
+
+NTP_CONTROL_OPCODES = {
+    "readstat" : 1,
+    "readvar"  : 2
+}
+
+# packet format to pack/unpack
+CONTROL_PACKET_FORMAT = "!B B H H H H H"
+
 
 class NTPException(Exception):
     """Exception raised by this module."""
     pass
 
 
-class NTPControlAssociation(object):
-
-    def __init__(self):
-        raise ValueError, "Don't use me"
-
-    def __str__(self):
-        return str({ k:v for (k, v) in self.__dict__.items() if not k.startswith('_')})
-
-    def decode(self, data):
-        """
-        Provided a 2 uchar of data, unpack the first uchar of associationID,
-        and the second uchar of association data from that uchar
-
-        test with  e.g. data set to:
-        In [161]: struct.pack("!B B", 0b00010100,0b00011010)
-        Out[161]: '\x14\x1a'
-        """
-        unpacked = struct.unpack("!H B B", data)
-
-        self.association_id = unpacked[0]
-
-        self.peer_config = unpacked[1] >> 7 & 0x1
-        self.peer_authenable = unpacked[1] >> 6 & 0x1
-        self.peer_authentic = unpacked[1] >> 5 & 0x1
-        self.peer_reach = unpacked[1] >> 4 & 0x1
-        self.reserved = unpacked[1] >> 3 & 0x1
-        self.peer_selection = unpacked[1] & 0x7
-
-        self.peer_event_counter = unpacked[2] >> 4 & 0xf
-        self.peer_event_code = unpacked[2] & 0xf
-
-
-
-class NTPControlPacket(object):
-    """NTP control packet class.
-
-    This represents an NTP control packet.
-    """
-
-    _PACKET_FORMAT = "!B B H H H H H" #Maybe, maybe not
-    """packet format to pack/unpack"""
-
-    _OPCODES = {
-        "readstat" : 1,
-        "readvar"  : 2
-    }
-
-    def __init__(self, version=2, op="readstat", association_id=0, sequence=1):
-        """Constructor.
-
-        Parameters:
-        version      -- NTP version
-        mode         -- packet mode (control, aka 6)
-        tx_timestamp -- packet transmit timestamp
-        """
-        self.leap = 0
-        """leap second indicator"""
-        self.version = version
-        """version"""
-        self.mode = 6
-        """mode"""
-        self.response_bit = 0 # request
-        self.error_bit = 0
-        self.more_bit = 0
-        self.opcode = NTPControlPacket._OPCODES[op]
-        self.sequence = sequence
-        self.status = 0
-        self.association_id = association_id
-        self.offset = 0
-        self.count = 0
-
-
-    def to_data(self):
-        """Convert this NTPControlPacket to a buffer that can be sent over a socket.
-
-        Returns:
-        buffer representing this packet
-
-        Raises:
-        NTPException -- in case of invalid field
-        """
-        try:
-            packed = struct.pack(
-                NTPControlPacket._PACKET_FORMAT,
-                (self.leap << 6 | self.version << 3 | self.mode),
-                (self.response_bit << 7 | self.error_bit << 6 |
-                 self.more_bit << 5 | self.opcode),
-                self.sequence,
-                self.status,
-                self.association_id,
-                self.offset,
-                self.count)
-        except struct.error:
-            raise NTPException("Invalid NTP packet fields.")
-        return packed
-
-    def from_data(self, data):
-        """Populate this instance from a NTP packet payload received from
-        the network.
-
-        Parameters:
-        data -- buffer payload
-
-        Raises:
-        NTPException -- in case of invalid packet format
-        """
-        try:
-            header_len = struct.calcsize(NTPControlPacket._PACKET_FORMAT)
-            unpacked = struct.unpack(NTPControlPacket._PACKET_FORMAT,
-                data[0:header_len])
-        except struct.error:
-            raise NTPException("Invalid NTP packet.")
-
-        # header status
-        self.leap = unpacked[0] >> 6 & 0x1
-        self.version = unpacked[0] >> 3 & 0x7
-        self.mode = unpacked[0] & 0x7  # end first uchar
-
-        self.response_bit = unpacked[1] >> 7 & 0x1
-        self.error_bit = unpacked[1] >> 6 & 0x1
-        self.more_bit = unpacked[1] >> 5 & 0x1
-        self.opcode = unpacked[1] & 0x1f  # end second uchar
-
-        self.sequence = unpacked[2]
-
-        # Another status (what do the docs call this?)
-        # only use the true/false bit somehow? don't get into more detail
-        self.leap = unpacked[3] >> 14 & 0x1
-        self.clocksource = unpacked[3] >> 8 & 0x1f  # 6 bit mask
-        self.system_event_counter = unpacked[3] >> 4 & 0xf
-        self.system_event_code = unpacked[3] & 0xf  # End first ushort
-
-        self.association_id = unpacked[4]
-        self.offset = unpacked[5]
-        self.count = unpacked[6]
-
-        opcodes_by_number = { v:k for k,v in NTPControlPacket._OPCODES.items() }
-        if opcodes_by_number[self.opcode] == "readstat":
-            self.decode_readstat(header_len,  data)
-        elif opcodes_by_number[self.opcode] == "readvar":
-            self.decode_readvar(header_len,  data)
-
-    def decode_readstat(self, header_len, data):
-        self.data = list()
-        for offset in range(header_len, len(data), 4):
-            assoc = data[offset:offset+4]
-            association_dict = decode_association(assoc)
-            self.data.append(association_dict)
-
-    def decode_readvar(self, header_len, data):
-        """From libntpq.h in the ntp distribution:
-/* NTP Status codes */
-#define NTP_STATUS_INVALID      0
-#define NTP_STATUS_FALSETICKER  1
-#define NTP_STATUS_EXCESS       2
-#define NTP_STATUS_OUTLIER      3
-#define NTP_STATUS_CANDIDATE    4
-#define NTP_STATUS_SELECTED     5
-#define NTP_STATUS_SYSPEER      6
-#define NTP_STATUS_PPSPEER      7
-
-        """
-
-
-        # TODO:  encode data here
-        buf = data[header_len:].split(",")
-        self.data = dict()
-        for d in buf:
-            key, val = d.replace("\r\n", "").lstrip().split("=")
-            if key in ('rec', 'reftime'):
-                int_part, frac_part = map(
-                    lambda x: int(x, 16), val.split("."))
-                self.data[key] = ntplib.ntp_to_system_time(
-                    ntplib._to_time(int_part, frac_part))
-            else:
-                self.data[key] = val
-        # For the equivalent of the 'when' column, in ntpq -c pe
-        # I believe that the time.time() minus the 'rec' field will give that.
-        self.data['when'] = time.time() - self.data['rec']
-
-
-class NTPControlClient(object):
-    """NTP client session."""
-
-    def __init__(self):
-        """Constructor."""
-        pass
-
-    def request(self, host, version=2, port='ntp', op="readvar", association_id=0, timeout=5):
-        """Query a NTP server.
-
-        Parameters:
-        host    -- server name/address
-        version -- NTP version to use
-        port    -- server port
-        timeout -- timeout on socket operations
-
-        Returns:
-        NTPStats object ??? XXX
-        """
-        # lookup server address
-        addrinfo = socket.getaddrinfo(host, port)[0]
-        family, sockaddr = addrinfo[0], addrinfo[4]
-
-        # create the socket
-        s = socket.socket(family, socket.SOCK_DGRAM)
-
-        try:
-            s.settimeout(timeout)
-
-            # create the request packet - mode 3 is client
-            query_packet = NTPControlPacket(
-                op=op, association_id=association_id)
-
-            # send the request
-            s.sendto(query_packet.to_data(), sockaddr)
-
-            # wait for the response - check the source address
-            src_addr = None,
-            # Will there ever be enough control info to need to
-            # concat multiple recvfroms()?
-            while src_addr[0] != sockaddr[0]:
-                response_packet, src_addr = s.recvfrom(512)
-
-            # build the destination timestamp
-            dest_timestamp = ntplib.system_to_ntp_time(time.time())
-        except socket.timeout:
-            raise NTPException("No response received from %s." % host)
-        finally:
-            s.close()
-
-        ncp = NTPControlPacket()
-        ncp.from_data(response_packet)
-        return ncp
-
-
 def composite_associnfo(host="127.0.0.1"):
     """
-    returns a list of associations from the host,
+    returns a list of assoc)iations from the host,
     this data is a mixture of the data that is gotten
     from the commands 'ntpq -c pe' and 'ntpq -c as'
 
     """
-    ncc = NTPControlClient()
-    ncp = ncc.request(host, op="readstat")
+    # ncc = NTPControlClient()
+    # ncp = ncc.request(host, op="readstat")
     data = list()
-    for assoc in ncp.data:
-        readvar_data = ncc.request(
+    ncp_data = ntp_control_request(host, op="readstat")
+    for assoc in ncp_data['associations']:
+        readvar_data = ntp_control_request(
             host, op="readvar", association_id=assoc['association_id'])
         for k, v in assoc.items():
-            readvar_data.data[k] = v
+            readvar_data[k] = v
         data.append(readvar_data)
     return data
 
@@ -317,3 +93,167 @@ def decode_association(data):
         'peer_event_counter' : unpacked[2] >> 4 & 0xf,
         'peer_event_code' : unpacked[2] & 0xf
     }
+
+
+def control_packet_data(version=2, op='readstat', association_id=0, sequence=1):
+    """Convert this NTPControlPacket to a buffer that can be sent over a socket.
+
+    Returns:
+    buffer representing this packet
+
+    Raises:
+    NTPException -- in case of invalid field
+    """
+    leap           = 0       # leap second indicator
+    version        = version # protocol version
+    mode           = 6       # mode 6 is the control mode
+    response_bit   = 0       # request
+    error_bit      = 0
+    more_bit       = 0
+    opcode         = NTP_CONTROL_OPCODES[op]
+    sequence       = sequence
+    status         = 0
+    association_id = association_id
+    offset         = 0
+    count          = 0
+    try:
+        packed = struct.pack(
+            NTP_CONTROL_PACKET_FORMAT,
+            (leap << 6 | version << 3 | mode),
+            (response_bit << 7 | error_bit << 6 | more_bit << 5 | opcode),
+            sequence,
+            status,
+            association_id,
+            offset,
+            count)
+        return packed
+    except struct.error:
+        raise NTPException("Invalid NTP packet fields.")
+
+
+def ntp_control_request(host, version=2, port='ntp',  # pylint: disable=too-many-arguments,invalid-name
+                        op="readvar", association_id=0, timeout=5):
+    """Query a NTP server.
+
+    Parameters:
+    host    -- server name/address
+    version -- NTP version to use
+    port    -- server port
+    timeout -- timeout on socket operations
+
+    Returns:
+    NTPStats object ??? XXX
+    """
+    # lookup server address
+    addrinfo = socket.getaddrinfo(host, port)[0]
+    family, sockaddr = addrinfo[0], addrinfo[4]
+
+    # create the socket
+    s = socket.socket(family, socket.SOCK_DGRAM)
+
+    try:
+        s.settimeout(timeout)
+
+        # create the request packet - mode 3 is client
+        s.sendto(
+            control_packet_data(op=op, version=version,
+                                association_id=association_id),
+            sockaddr)
+
+        # wait for the response - check the source address
+        src_addr = None,
+        # Will there ever be enough control info to need to
+        # concat multiple recvfroms()?
+        while src_addr[0] != sockaddr[0]:
+            response_packet, src_addr = s.recvfrom(512)
+
+        # build the destination timestamp
+        # dest_timestamp = ntplib.system_to_ntp_time(time.time())
+    except socket.timeout:
+        raise NTPException("No response received from %s." % host)
+    finally:
+        s.close()
+
+    packet_dict = control_packet_from_data(response_packet)
+    return packet_dict
+
+
+
+def control_packet_from_data(data):
+    """Populate this instance from a NTP packet payload received from
+    the network.
+
+    Parameters:
+    data -- buffer payload
+
+    Returns:
+    dictionary of control packet data.
+
+    Raises:
+    NTPException -- in case of invalid packet format
+    """
+    try:
+        header_len = struct.calcsize(CONTROL_PACKET_FORMAT)
+        unpacked = struct.unpack(CONTROL_PACKET_FORMAT,
+            data[0:header_len])
+    except struct.error:
+        raise NTPException("Invalid NTP packet.")
+
+    # header status
+    rdata = {
+        "leap_header"          : unpacked[0] >> 6 & 0x1,
+        "version"              : unpacked[0] >> 3 & 0x7,
+        "mode"                 : unpacked[0] & 0x7,  # end first uchar
+        "response_bit"         : unpacked[1] >> 7 & 0x1,
+        "error_bit"            : unpacked[1] >> 6 & 0x1,
+        "more_bit"             : unpacked[1] >> 5 & 0x1,
+        "opcode"               : unpacked[1] & 0x1f,  # end second uchar
+        "sequence"             : unpacked[2],
+        # Another status (what do the docs call this?)
+        # only use the true/false bit somehow? don't get into more detail
+        "leap"                 : unpacked[3] >> 14 & 0x1,
+        "clocksource"          : unpacked[3] >> 8 & 0x1f,  # 6 bit mask
+        "system_event_counter" : unpacked[3] >> 4 & 0xf,
+        "system_event_code"    : unpacked[3] & 0xf,  # End first ushort
+        "association_id"       : unpacked[4],
+        "offset"               : unpacked[5],
+        "count"                : unpacked[6]
+    }
+
+    opcodes_by_number = { v:k for k,v in NTP_CONTROL_OPCODES.items() }
+    if opcodes_by_number[rdata['opcode']] == "readstat":
+        return decode_readstat(header_len,  data, rdata)
+    elif opcodes_by_number[rdata['opcode']] == "readvar":
+        return decode_readvar(header_len,  data, rdata)
+
+def decode_readstat(header_len, data, rdata):
+    """
+    Decodes a readstat request.  Augments rdata with
+    association IDs from data
+    """
+    rdata['associations'] = list()
+    for offset in range(header_len, len(data), 4):
+        assoc = data[offset:offset+4]
+        association_dict = decode_association(assoc)
+        rdata['associations'].append(association_dict)
+    return rdata
+
+# not done yet
+def decode_readvar(header_len, data, rdata):
+    """
+    Decodes a redvar request.  Augments rdata dictionary with
+    the textual data int he data packet.
+    """
+    buf = data[header_len:].split(",")
+    for d in buf:
+        key, val = d.replace("\r\n", "").lstrip().split("=")
+        if key in ('rec', 'reftime'):
+            int_part, frac_part = [ int(x, 16) for x in val.split(".") ]
+            rdata[key] = ntplib.ntp_to_system_time(
+                ntplib._to_time(int_part, frac_part))  # pylint: disable=protected-access
+        else:
+            rdata[key] = val
+    # For the equivalent of the 'when' column, in ntpq -c pe
+    # I believe that the time.time() minus the 'rec' field will give that.
+    rdata['when'] = time.time() - rdata['rec']
+    return rdata
